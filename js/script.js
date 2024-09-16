c8f69bb0be28a1bd63ed32724731d9d3621863d1
gatteoelmo <matteogallardo99@gmail.com>
2024-09-16 12:03:36 +0100

    axios

--- js/script.js
+++ js/script.js
@@ -1,96 +1,114 @@
+import axios from 'axios';
+
 const main = document.querySelector("#main");
-/* initializing a variable `lastLoadedIndex` with an initial
-value of 0. This variable is used to keep track of the index of the last loaded story from the
-Hacker News API. It is incremented by 10 each time a batch of stories is loaded, so that the next
-batch can start from the correct index in the data array. This helps in fetching and displaying
-stories in batches rather than all at once. */
-let lastLoadedIndex = 0; 
+let lastLoadedIndex = 0;
 
+console.log("Caricamento in corso...");
 
-/**
- * The function `getNews` fetches new stories from Hacker News API, processes the data, and displays
- * the stories on the webpage.
- */
-function getNews() {
-    // Fetch the new stories from the Hacker News API
-    fetch("https://hacker-news.firebaseio.com/v0/newstories.json")
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`Error fetching data: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then((data) => {
-            console.log("Fetched data:", data);
-            // Determine the IDs of the articles to load
-            const idsToLoad = data.slice(lastLoadedIndex, lastLoadedIndex + 10);
-            lastLoadedIndex += 10; // Update the index for the next batch of articles
-            const storyPromises = idsToLoad.map((id) => fetchStory(id));
-            return Promise.all(storyPromises);
-        })
-        .then((stories) => {
-            console.log("Fetched story data:", stories);
-            stories.forEach((story) => {
-                const container = document.createElement("div");
-                container.classList.add("container");
-                container.addEventListener("click", () => {
-                    window.open(story.url, "_blank");
-                })
-                
-                // Add the news title
-                const newsTitle = document.createElement("div");
-                newsTitle.classList.add("newsTitle");
-                newsTitle.innerHTML = story.title;
-                container.appendChild(newsTitle);
-
-                // Add the news url
-                const newsUrl = document.createElement("div");
-                newsUrl.classList.add("newsUrl");
-                newsUrl.innerHTML = `${story.url}`;
-                container.appendChild(newsUrl);
-
-                // Add the news date
-                const date = new Date(story.time * 1000);
-                const newsDate = document.createElement("div");
-                newsDate.classList.add("newsDate");
-                newsDate.innerHTML = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
-                container.appendChild(newsDate);
-
-                main.appendChild(container);
-            });
-        })
-        .catch((error) => {
-            console.error("Error fetching data:", error);
-        });
+// Funzione per recuperare gli ID delle nuove storie
+async function fetchNewStoryIds() {
+    try {
+        const response = await axios.get("https://hacker-news.firebaseio.com/v0/newstories.json");
+        return response.data;
+    } catch (error) {
+        console.error(`Errore durante il recupero degli ID delle storie: ${error}`);
+        throw error;
+    }
 }
 
-// function to fetch a single story
-function fetchStory(id) {
-    return fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`Error fetching story: ${response.status}`);
-            }
-            return response.json();
-        });
+// Funzione per recuperare una singola storia per ID
+async function fetchStory(id) {
+    try {
+        const response = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
+        return response.data;
+    } catch (error) {
+        console.error(`Errore durante il recupero della storia con ID ${id}: ${error}`);
+        throw error;
+    }
 }
 
-// Function to create the button
-function makeButton() {
-    console.log("Function makeButton called");
-    const loadMore = document.createElement("button");
-    loadMore.classList.add("container");
-    loadMore.innerHTML = "Load More";
-    main.insertAdjacentElement('beforeend', loadMore);
-    console.log("Button Load More created correctly");
+// Funzione per caricare e visualizzare le storie
+async function loadStories() {
+    try {
+        const allIds = await fetchNewStoryIds();
+        if (allIds.length === 0) {
+            displayNoNewsMessage();
+            return;
+        }
 
-    loadMore.addEventListener("click", () => {
-        loadMore.remove();
-        getNews();
-        setTimeout(makeButton, 2000);
+        const idsToLoad = allIds.slice(lastLoadedIndex, lastLoadedIndex + 10);
+        lastLoadedIndex += 10;
+        const stories = await Promise.all(idsToLoad.map(fetchStory));
+
+        if (stories.length === 0) {
+            displayNoNewsMessage();
+        } else {
+            displayStories(stories);
+        }
+    } catch (error) {
+        displayErrorMessage();
+    }
+}
+
+// Funzione per visualizzare le storie
+function displayStories(stories) {
+    stories.forEach(story => {
+        const container = document.createElement("div");
+        container.classList.add("container");
+        container.addEventListener("click", () => {
+            window.open(story.url, "_blank");
+        });
+
+        // Titolo della notizia
+        const newsTitle = document.createElement("div");
+        newsTitle.classList.add("newsTitle");
+        newsTitle.textContent = story.title;
+        container.appendChild(newsTitle);
+
+        // URL della notizia
+        const newsUrl = document.createElement("div");
+        newsUrl.classList.add("newsUrl");
+        newsUrl.textContent = story.url;
+        container.appendChild(newsUrl);
+
+        // Data della notizia
+        const date = new Date(story.time * 1000);
+        const newsDate = document.createElement("div");
+        newsDate.classList.add("newsDate");
+        newsDate.textContent = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+        container.appendChild(newsDate);
+
+        main.appendChild(container);
     });
 }
 
-// charge the news and the button
-getNews()
-setTimeout(makeButton, 2000);
+// Funzione per visualizzare un messaggio quando non ci sono notizie
+function displayNoNewsMessage() {
+    main.innerHTML = '<div class="message">Nessuna notizia trovata</div>';
+}
+
+// Funzione per visualizzare un messaggio di errore
+function displayErrorMessage() {
+    main.innerHTML = '<div class="message">Errore durante il recupero delle notizie</div>';
+}
+
+// Funzione per creare il pulsante "Carica di più"
+function createLoadMoreButton() {
+    console.log("Creazione del pulsante Carica di più");
+    const loadMore = document.createElement("button");
+    loadMore.classList.add("container");
+    loadMore.textContent = "+";
+    main.insertAdjacentElement('beforeend', loadMore);
+    console.log("Pulsante Carica di più creato correttamente");
+
+    loadMore.addEventListener("click", async () => {
+        loadMore.remove();
+        await loadStories();
+        createLoadMoreButton();
+    });
+}
+
+// Carica le notizie iniziali e crea il pulsante
+loadStories().then(() => {
+    createLoadMoreButton();
+});
